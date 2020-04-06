209db775f6642228e5715f3b2c1cb0b3671ec61d
Nitin <Nshirke007@gmail.com>
2020-04-06 12:52:10 +0530

    jenkins pipeline initial commit

---
+++ jenkinspipeline/src/main/java/com/demo/jenkinspipeline/jenkinspipeline/App.java
@@ -0,0 +1,13 @@
+package com.demo.jenkinspipeline.jenkinspipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
